83addac1d169ec870106e187616b00bfb9ccc7b8
aparent-git2 <113169785+aparent-git2@users.noreply.github.com>
2024-04-22 16:10:21 +0200

    Update buckets.tf

--- AWS-examples/buckets.tf
+++ AWS-examples/buckets.tf
@@ -13,7 +13,7 @@ resource "aws_s3_bucket" "state_terraform_s3" {
 
 resource "aws_s3_bucket_public_access_block" "state_terraform_s3" {
   bucket                  = aws_s3_bucket.state_terraform_s3.id
-  block_public_acls       = true
+  block_public_acls       = false
   block_public_policy     = false
   restrict_public_buckets = false
   ignore_public_acls      = false
